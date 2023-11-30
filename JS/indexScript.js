61f510572cbce1d761c0316b75916d01e44cd027
Jame-AS <aphinut.sue@dome.tu.ac.th>
2023-11-30 18:46:16 +0700

    change navigation code

---
+++ JS/indexScript.js
@@ -0,0 +1,11 @@
+document.getElementById("link1").addEventListener("click", function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById("link2").addEventListener("click", function() {
+    window.location.href = './html/OurActivity.html';
+});
+
+document.getElementById("link3").addEventListener("click", function() {
+    window.location.href = '';
+});
